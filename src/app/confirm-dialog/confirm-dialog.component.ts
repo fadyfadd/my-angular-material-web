8c32d8a6ddcfcea4b433a4189369abeb0a41b52a
fadyfadd <fady_faddoul@hotmail.com>
2020-03-20 15:36:03 +0200

    Cleanup

--- src/app/confirm-dialog/confirm-dialog.component.ts
+++ src/app/confirm-dialog/confirm-dialog.component.ts
@@ -12,7 +12,6 @@ export class ConfirmDialogComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<ConfirmDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: ConfirmDialogModel) {
-    // Update view with given values
     this.title = data.title;
     this.message = data.message;
   }
@@ -21,21 +20,14 @@ export class ConfirmDialogComponent implements OnInit {
   }
 
   onConfirm(): void {
-    // Close the dialog, return true
-    this.dialogRef.close(true);
+     this.dialogRef.close(true);
   }
 
   onDismiss(): void {
-    // Close the dialog, return false
-    this.dialogRef.close(false);
+     this.dialogRef.close(false);
   }
 }
-
-/**
- * Class to represent confirm dialog model.
- *
- * It has been kept here to keep it as part of shared component.
- */
+ 
 export class ConfirmDialogModel {
 
   constructor(public title: string, public message: string) {
